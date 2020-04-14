cea925853f797bc4dcce50a1d0617d9daf6cc89a
airtoxin <airtoxin@icloud.com>
2020-04-14 22:17:13 +0900

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import { paths } from "./paths";
+import { Root } from "./Root";
+import { CreateReactApp } from "./CreateReactApp";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
-
-export default App;
+export const App: React.FunctionComponent = () => (
+  <HashRouter>
+    <Switch>
+      <Route
+        path={paths["/"].routingPath}
+        component={Root}
+        exact
+      />
+      <Route
+        path={paths["/cra"].routingPath}
+        component={CreateReactApp}
+        exact
+      />
+    </Switch>
+  </HashRouter>
+);
---
+++ src/paths.ts
@@ -0,0 +1,8 @@
+export const paths = {
+  "/": {
+    routingPath: "/"
+  },
+  "/cra": {
+    routingPath: "/cra"
+  }
+};
